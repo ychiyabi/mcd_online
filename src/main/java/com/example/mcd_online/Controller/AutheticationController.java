83d6c3146f1ee87155f168db9d19d45dcbf683c3
yaschiyabi <ychiyabi@gmail.com>
2024-03-16 19:40:56 +0000

    Working on OAUTH 2

--- src/main/java/com/example/mcd_online/Controller/AutheticationController.java
+++ src/main/java/com/example/mcd_online/Controller/AutheticationController.java
@@ -5,31 +5,37 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.mcd_online.Event.LoginEvent;
 
-@RestController
+@Controller
 public class AutheticationController {
 
     @Autowired
     private ApplicationEventPublisher applicationEventPublisher;
 
     @GetMapping("/getGithubUser")
-    public String gitHubToken(@RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient client) {
+    public ModelAndView gitHubToken(@RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient client) {
 
         OAuth2AccessToken token = client.getAccessToken();
         LoginEvent event = new LoginEvent(client, client.getPrincipalName(), token.getTokenValue());
         this.applicationEventPublisher.publishEvent(event);
-        return "";
+        ModelAndView mv = new ModelAndView("authsuccess");
+        mv.getModel().put("token", token.getTokenValue());
+        return mv;
     }
 
     @GetMapping("/getGoogleUser")
-    public String getGoogleToken(@RegisteredOAuth2AuthorizedClient("google") OAuth2AuthorizedClient client) {
+    public ModelAndView getGoogleToken(@RegisteredOAuth2AuthorizedClient("google") OAuth2AuthorizedClient client) {
         OAuth2AccessToken token = client.getAccessToken();
         LoginEvent event = new LoginEvent(client, client.getPrincipalName(), token.getTokenValue());
         this.applicationEventPublisher.publishEvent(event);
-        return "";
+        ModelAndView mv = new ModelAndView("authsuccess");
+        mv.getModel().put("token", token.getTokenValue());
+        return mv;
     }
 }
